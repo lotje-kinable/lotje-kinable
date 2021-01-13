713b40faa00c636d6364833a1269a9f0255df959
kinablelieselot <lkinable@icloud.com>
2021-01-13 16:13:55 +0100

    bachelor_degree lol

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
         <div class="grid text-sm md:text-lg md:grid-cols-4 place-items-center md:gap-1 gap-1 grid-cols-2 rounded-xl bg-pink-100 p-4 md:p-6 mb-6 md:mb-12">
             <div class="hover:text-pink-500"><a href="https://laravel.com" target="_blank"><span class="text-pink-500">#</span>laravel</a></div>
             <div class=""><span class="text-purple-500">#</span>developer</div>
-            <div class=""><span class="text-blue-500">#</span>bachelor</div>
+            <div class=""><span class="text-blue-500">#</span>bachelor_degree</div>
             <div class="hover:underline hover:text-blue-400">
                 <a href="mailto:lkinable@gmail.com"><span class="text-red-500">#</span>hire_me!</a>
             </div>
