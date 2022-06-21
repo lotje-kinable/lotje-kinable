586c2218df6642c715847232a0501c5864ff4012
Lotje Kinable <lkinable@icloud.com>
2022-06-21 18:11:45 +0200

    updated tailwind to v3

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -42,7 +42,7 @@
 {{--                </svg>--}}
 {{--            </button>--}}
 {{--        </div>--}}
-{{--        <nav  :class="{'flex': open, 'hidden': !open}" class="flex-col flex-grow pb-4 md:pb-0 hidden md:flex md:justify-end md:flex-row md:gap-x-6 gap-y-4 md:gap-y-0">--}}
+{{--        <nav  :class="{'flex': open, 'hidden': !open}" class="flex-col grow pb-4 md:pb-0 hidden md:flex md:justify-end md:flex-row md:gap-x-6 gap-y-4 md:gap-y-0">--}}
 {{--            @if ( request()->is('posts.*') )--}}
 {{--                <a--}}
 {{--                    --}}{{--            class="px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-gray-200 rounded-lg dark-mode:bg-gray-700 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"--}}
